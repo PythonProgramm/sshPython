93fce59090ff3c09f0c1aaf1fa29071b76754e0a
HACK3R <root@YOUVEBEENHACKED>
2021-01-12 08:05:33 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name="sshPython",
+    author="Frederik Levi Kursawe",
+    author_email="frederik.kursawe@icloud.com",
+    scripts=["sshpython.py"],
+    version="1.0"
+)
